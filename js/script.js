99d1a7d1dbec276d7254f5bcd4cf19e9247937ac
Michał Flis <michal.flis@op.pl>
2021-08-10 19:04:00 +0200

    finished handler event

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@
   });*/
 
   const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
   /* [DONE] remove class 'active' from all article links  */
@@ -18,17 +19,18 @@
   /*[IN PROGRESS] add class 'active' to the clicked link */
   console.log('clickedElement:', clickedElement);
   /*[DONE] remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.title article.active');
-
+  const activeArticles = document.querySelectorAll('article.active');
+  console.log(activeArticles)
   for(let activeArticle of activeArticles){
-  activeLink.classList.remove('active');
+  activeArticle.classList.remove('active');
   }
   /* get 'href' attribute from the clicked link */
-
+  const articleSelector = clickedElement.getAttribute('href');
   /* find the correct article using the selector (value of 'href' attribute) */
-
+  const targetArticle = document.querySelector(articleSelector);
   /* add class 'active' to the correct article */
-  }
+  targetArticle.classList.add('active');
+}
   
   const links = document.querySelectorAll('.titles a');
   
